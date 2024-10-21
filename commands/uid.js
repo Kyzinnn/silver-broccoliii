a8eebb501a368c68152972c831a6cabe84ef00d2
Rocket-Launcher101 <168435552+Rocket-Launcher101@users.noreply.github.com>
2024-10-21 19:58:35 +0800

    Update uid.js

--- commands/uid.js
+++ commands/uid.js
@@ -1,4 +1,7 @@
 const { sendMessage } = require('../handles/sendMessage');
+const fs = require('fs');
+
+const token = fs.readFileSync('token.txt', 'utf8');
 
 module.exports = {
   name: 'uid',
@@ -7,11 +10,13 @@ module.exports = {
   author: 'coffee',
 
   async execute(senderId) {
+    const pageAccessToken = token;
+
     try {
-      await sendMessage(senderId, { text: `Your UID is: ${senderId}` });
+      await sendMessage(senderId, { text: `${senderId}` }, pageAccessToken);
     } catch (error) {
       console.error('Error sending UID:', error);
-      await sendMessage(senderId, { text: 'Error: Unable to retrieve your UID.' });
+      await sendMessage(senderId, { text: 'Error: Unable to retrieve your UID.' }, pageAccessToken);
     }
   },
 };
